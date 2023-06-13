6675dfbe128219f6785e6ad313be76b6e29f75c6
Ana <anarosa.af@outlook.com>
2023-06-13 13:24:06 -0300

    criação do projeto com react e pag home

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+  return (
+    <>
+      <h1 className='titulo'>Home</h1>
+      <img src="https://i.imgur.com/H88yIo2.png" alt="Imagem tele inicial" className='imgHome'/>
+
+    </>
+  );
+}
+
+export default Home;
