6542d80c2badc4d47fc349462b9419b47eafa2af
Ana <anarosa.af@outlook.com>
2023-06-23 17:42:01 -0300

    refatorizacao e estilizacao da navbar

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -2,47 +2,48 @@ import { AppBar, Toolbar, Typography } from "@material-ui/core";
 import { Box } from "@mui/material";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import logout from '../../svg/icon/bx-log-out.svg';
+import logo from '../../svg/carambola-svgrepo-com.svg'
 
 export default function Navbar() {
   return (
     <>
-      <AppBar position="static">
-        <Toolbar variant="dense">
+      <AppBar position="static" className="navegacao">
+        <Toolbar variant="dense" className="sb">
           <Box className="cursor">
-            <Typography variant="h5" color="inherit">
-              BlogPessoal
-            </Typography>
+            <img className="logo" src={logo} alt="desenho de um pedaço de carambola" />
           </Box>
 
-          <Box display="flex" justifyContent="start">
+          <Box className="sb">
             <Box mx={1} className="cursor">
-              <Typography variant="h6" color="inherit">
-                Home
+              <Typography className='menuText'>
+                HOME
               </Typography>
             </Box>
             <Box mx={1} className="cursor">
-              <Typography variant="h6" color="inherit">
-                Postagens
+              <Typography variant="h6" className='menuText'>
+                POSTAGENS
               </Typography>
             </Box>
             <Box mx={1} className="cursor">
-              <Typography variant="h6" color="inherit">
-                Temas
+              <Typography variant="h6" className='menuText'>
+                TEMAS
               </Typography>
             </Box>
             <Box mx={1} className="cursor">
-              <Typography variant="h6" color="inherit">
-                Novo Tema
+              <Typography variant="h6" className='menuText'>
+                NOVO TEMA
               </Typography>
             </Box>
-            <Link to="/login" className="text-decorator-none">
-              <Box mx={1} className="cursor">
-                <Typography variant="h6" color="inherit">
-                  Logout
-                </Typography>
-              </Box>
-            </Link>
           </Box>
+          <Link to="/login" className="text-decorator-none">
+            <Box mx={1} className="cursor logout" >
+              <Typography variant="h6">
+                LOGOUT
+              </Typography>
+              <img src={logout} alt="um retângulo como uma porta e uma seta saindo dele" />
+            </Box>
+          </Link>
         </Toolbar>
       </AppBar>
     </>
