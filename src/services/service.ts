e88261c0260704b920f09ec26c4e9eb595a43326
Ana <anarosa.af@outlook.com>
2023-06-26 23:15:39 -0300

    criacao do cadastro usuario

--- src/services/service.ts
+++ src/services/service.ts
@@ -4,6 +4,11 @@ export const api = axios.create({
   baseURL: 'https://blogpessoal-4ccg.onrender.com/swagger-ui/index.html'
 })
 
+export const cadastroUsuario = async (url: any, dados: any, setDado: any) => {
+  const resposta = await api.post(url, dados)
+  setDado(resposta.data)
+}
+
 export const login = async(url: any, dados: any, setDado: any) => {
   const resposta = await api.post(url,dados)
   setDado(resposta.data.token)
