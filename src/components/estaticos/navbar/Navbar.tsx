c54ee2db2e1b4c836ae126192836e41a3b270ab3
Ana <anarosa.af@outlook.com>
2023-06-27 01:49:49 -0300

    refatoracao da navbar com adicao de rotas

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,11 +1,21 @@
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
 import { Box } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Navbar.css";
 import logout from '../../svg/icon/bx-log-out.svg';
 import logo from '../../svg/carambola-svgrepo-com.svg'
+import useLocalStorage from "react-use-localstorage";
 
 export default function Navbar() {
+  const [token, setToken] = useLocalStorage('token');
+  const navigator = useNavigate();
+
+  function goLogout() {
+    setToken('')
+    alert('Usuário deslogado!')
+    navigator('/login')
+  }
+
   return (
     <>
       <AppBar position="static" className="navegacao">
@@ -15,35 +25,41 @@ export default function Navbar() {
           </Box>
 
           <Box className="sb">
+            <Link to='/home' className="text-decoration-none">
             <Box mx={1} className="cursor">
               <Typography className='menuText'>
                 HOME
               </Typography>
             </Box>
+            </Link>
+            <Link to='/posts' className="text-decoration-none">
             <Box mx={1} className="cursor">
               <Typography variant="h6" className='menuText'>
                 POSTAGENS
               </Typography>
             </Box>
+            </Link>
+            <Link to='/temas' className="text-decoration-none">
             <Box mx={1} className="cursor">
               <Typography variant="h6" className='menuText'>
                 TEMAS
               </Typography>
             </Box>
+            </Link>
+            <Link to='/formularioTema' className="text-decoration-none">
             <Box mx={1} className="cursor">
               <Typography variant="h6" className='menuText'>
                 NOVO TEMA
               </Typography>
             </Box>
+            </Link>
           </Box>
-          <Link to="/login" className="text-decorator-none">
-            <Box mx={1} className="cursor logout" >
+            <Box mx={1} className="cursor logout" onClick={goLogout}>
               <Typography variant="h6">
                 LOGOUT
               </Typography>
               <img src={logout} alt="um retângulo como uma porta e uma seta saindo dele" />
             </Box>
-          </Link>
         </Toolbar>
       </AppBar>
     </>
